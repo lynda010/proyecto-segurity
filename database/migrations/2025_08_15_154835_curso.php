696e56f6ba128c824ec5484ec43afc5498b0583b
Lynda Pinto <lyndajulyanapintomeza@gmail.com>
2025-09-18 07:48:54 -0500

    primer commit

---
+++ database/migrations/2025_08_15_154835_curso.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('curso',function (Blueprint $table){
+            $table ->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->string('duracion');
+            $table->decimal('precio',8,2);
+            
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
